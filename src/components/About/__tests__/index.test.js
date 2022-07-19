d6548d13ea6f9d57b8f50b797deebf5e7c9163ce
Chrisborer <chris.borer@gmail.com>
2022-07-19 12:20:49 -0500

    finished section 20.5

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,3 +1,4 @@
+// __tests__/About.test.js
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
@@ -6,13 +7,13 @@ import About from '..';
 afterEach(cleanup);
 
 describe('About component', () => {
-    // First Test
   it('renders', () => {
     render(<About />);
   });
-  it('matches snapshot DOM node structure', () => {
-    // render About
+
+  it('matches snapshot', () => {
+    const { asFragment } = render(<About />);
+
+    expect(asFragment()).toMatchSnapshot();
   });
 })
-const { asFragment } = render(<About />);
-expect(asFragment()).toMatchSnapshot();
